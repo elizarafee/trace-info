dbfd9d1a6a938303831929f555dba8c276014fd6
Eliza Ahmed <elizarafee@gmail.com>
2020-06-26 17:00:15 +0600

    added error messages : storpeoplerequest

--- app/Http/Requests/StorePeopleRequest.php
+++ app/Http/Requests/StorePeopleRequest.php
@@ -36,6 +36,24 @@ class StorePeopleRequest extends FormRequest
         ];
     }
 
-    
-
+    public function messages()
+    {
+        return[
+            'first_name.required' => 'First name is required',
+            'first_name.max' => "First name field's max length is 30",
+            'last_name.required' => 'Last name is required',
+            'last_name.max' => "Last name field's max length is 30",
+            'address_line_1.required' => 'Address is required',
+            'address_line_1.max' => "Address field's max length is 100",
+            'address_line_2.max' => "Street field's max length is 100",
+            'city.required' => 'City is required',
+            'city.max' => "City field's max length is 30",
+            'lng.max' => "Longitude field's max length is 20",
+            'lat.max' => "Lattitude field's max length is 100",
+            'email.required' => 'Email is required',
+            'email.max' => "Email field's max length is  70",
+            'phone.required' => 'Phone number is required',
+            'phone.max' => "Phone number's field's max length is",
+        ];
+    }
 }
